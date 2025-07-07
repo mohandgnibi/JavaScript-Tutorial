d2d5e0c8746a507c880effc419f6a68b7d79adbd
vostro <vostro@example.com>
2025-07-07 21:47:14 +0100

    Code quality: Coding Style
    Add coding style HTML and JavaScript files

---
+++ JavaScript.info/2-Code-quality/2-coding-style/coding_style.js
@@ -0,0 +1,21 @@
+// Coding Style
+
+/**
+ * Our code must be as clean and easy to read as possible
+ * 
+ * That is actually the art of programming – to take a complex task and code it 
+ * in a way that is both correct and human-readable. A good code style greatly 
+ * assists in that.
+ */
+
+// ======================== Syntax ========================//
+
+// Here is a cheat sheet with some suggested rules (see below for more details):
+
+/**
+ *               function pow(x, n) {
+ *                 let result = 1;
+ * 
+ *                 for (let i = 0)
+ *              }
+ */
